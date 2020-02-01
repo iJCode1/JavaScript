e16ffe72423252df44d95a7fda469b08d5fa880e
Joel Dominguez Merino <joeldome17@gmail.com>
2020-01-31 20:24:40 -0600

    Constantes en JavaScript

---
+++ 2.Javascript/4-constantes.js
@@ -0,0 +1,11 @@
+"use strict"
+
+//Constantes en Javascript
+//Las constantes se usan para asignar valores que jamas podran cambiar a lo largo de nuestro programa
+//Para declarar una constante se utiliza la palabra reservada const
+
+var nombre="Joel"; //Variable : Puede cambiar 
+const edad = 20; //Constante: No puede cambiar
+
+console.log("Nombre: "+nombre);
+console.log("Edad: "+edad);
