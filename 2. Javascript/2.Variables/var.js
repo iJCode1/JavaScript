a1d60dabe7067e202f12e403ec00002e47b7a96b
Joel Dominguez Merino <joeldome17@gmail.com>
2020-01-31 19:41:17 -0600

    Variables en Javascript con Var

---
+++ 2. Javascript/2.Variables/var.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var pais = "México";
+var continente = "Americano";
+var cadena;
+
+cadena = pais+' '+continente;
+
+console.log("El pais es: "+pais);
+alert("La cadena es: "+cadena);
