a16db055d7eaba0e1825227d29c031fbf9335847
Joel Dominguez <joeldome17@gmail.com>
2020-02-02 14:45:19 -0600

    Bucle For en JS

---
+++ 2.Javascript/8-for.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Bucle FOR!
+
+var tabla = 7;
+
+document.writeln("La tabla del numero: "+tabla+" es...")
+for(var i=1;i<=10;i++){
+    document.writeln("<br/>"+tabla+"*"+i+"="+(tabla*i));
+}
