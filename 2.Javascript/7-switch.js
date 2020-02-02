4c4f2d5622bc305fd5ab69df2e41034380739f3f
Joel Dominguez <joeldome17@gmail.com>
2020-02-02 14:34:03 -0600

    Switch en JS

---
+++ 2.Javascript/7-switch.js
@@ -0,0 +1,18 @@
+// Esctructura Switch en JS
+
+var n = 3;
+
+switch(n){
+    case 1:
+        console.log("Numero 1!");
+        break;
+    case 2:
+        console.log("Numero 2!");
+        break;
+    case 3:
+        console.log("Numero 3");
+        break;
+    default:
+        console.log("Caso no encontrado!");
+        break;
+}
