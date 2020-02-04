85e71156c8272840357d8503a781ec13614020c9
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-03 22:55:20 -0600

    agregue mas logica a mi primer ejercicio

--- 2.Javascript/11-ej1_2numeros.js
+++ 2.Javascript/11-ej1_2numeros.js
@@ -3,19 +3,31 @@
 /*
 	Pedir 2 numeros al Usuarios
 	Mostrar que numero es mayor, menor y si son iguales
+	Extra: Si los numeros no son un numero o son menores o igual a cero, volverlos a pedir
 */
 
-var n1,n2;
+	var n1,n2;
 
-n1 = prompt("Ingresa el valor del Numero1:");
-n2 = prompt("Ingresa el valor del Numero2:");
+	n1 = parseInt(prompt("Ingresa el valor del Numero1:"));
+	n2 = parseInt(prompt("Ingresa el valor del Numero2:"));
 
-document.write("Los numeros son:<br/>Numero1: "+n1+"<br/>Numero2:"+n2+"<br/>");
+	while((n1<=0 || n2<=0) || (isNaN(n1) || isNaN(n2) )){
+		n1 = parseInt(prompt("Ingresa el valor del Numero1:"));
+		n2 = parseInt(prompt("Ingresa el valor del Numero2:"));
+	}
 
-if(n1>n2){
-	document.write(n1+" es mayor que "+n2);
-}else if(n1<n2){
-	document.write(n1+" es menor que "+n2);
-}else{
-	document.write("Ambos numeros soon iguales... valor= "+n1);
-}
+	document.write("Los numeros son:<br/>Numero1: <b>"+n1+"</b><br/>Numero2: <b>"+n2+"</b><br/>");
+
+	if(n1>n2){
+		document.write(n1+" es mayor que "+n2);
+	}else if(n1<n2){
+		document.write(n1+" es menor que "+n2);
+	}else if(n1==n2){
+		document.write("Ambos numeros son iguales... valor= "+n1);
+	}else{
+		document.write("Introduzca valores numericos");
+	}
+
+
+
+
