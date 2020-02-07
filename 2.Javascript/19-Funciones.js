8fae519f8b4f4c6a6fb5e65e409244c935c4491a
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-07 07:58:05 -0600

    Funciones en JS

---
+++ 2.Javascript/19-Funciones.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+
+	Funciones en Javascript
+
+	Una funcion es un conjunto de Instrucciones que pueden ser llamadas las veces que queramos.
+
+*/
+
+function Operaciones(n1,n2){
+	document.write("<p>Numero 1: <b>"+n1+"</b>, Numero 2: <b>"+n2+"</b></p>")
+	document.write("<p>La suma es: "+(n1+n2)+"</p>");
+	document.write("<p>La resta es: "+(n1-n2)+"</p>");
+	document.write("<p>La multiplicación es: "+(n1*n2)+"</p>");
+	document.write("<p>La división es: "+(n1/n2)+"</p>");
+	document.write("************************************************")
+}
+
+Operaciones(7,3);
+Operaciones(5,10);
