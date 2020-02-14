7a70129d07c4769ae8d5e727cd499fff5821ee30
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-13 19:45:30 -0600

    Funciones Anonimas en JS

---
+++ 2.Javascript/21-funcionesAnonimas_callbacks.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+	En este archivo tratare de implementar las funciones anonimas y los Callbacks.
+
+		Las funciones anonimas son funciones que no tienen un nombre, son guardadas dentro de una variable.
+		llamandolas con el nombre de la variable...
+
+
+*/
+
+var msj = function(nombre){
+	document.write("Hola, soy una función Anonima. y tu eres: "+nombre);
+};
+
+msj("Joel");
