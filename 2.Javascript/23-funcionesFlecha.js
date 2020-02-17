ecd51b887e2dc225328a999c0f17a979c236b75c
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-16 23:16:09 -0600

    Repaso - Funciones Flecha con Callbacks

--- 2.Javascript/23-funcionesFlecha.js
+++ 2.Javascript/23-funcionesFlecha.js
@@ -9,8 +9,22 @@
 
 	//Repaso de Funciones Flecha!
 
+/*
+
 var nameV = (nameP,edad) =>
 	document.write("Name is: "+nameP+" Tiene "+edad+" años.");
 
 
 nameV("Joel",20);
+
+*/
+
+	//Repaso de Funciones Flecha pero con Callbacks.
+
+function cal(n1,n2,suma){
+	document.write("<p>Number 1: "+n1+"</p>");
+	document.write("<p>Number 2: "+n2+"</p>");
+	suma(n1+n2);
+}
+
+cal(7,2,(dato)=> document.write("<p>Suma: "+dato+"</p>"));
