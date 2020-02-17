75819dca53d3678cd9da7d8618fd4df150c4fc58
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-16 23:07:29 -0600

    Tercer repaso de Funciones Anonimas y Callbacks

--- 2.Javascript/22-repasoFunciones_anonimas_callbacks.js
+++ 2.Javascript/22-repasoFunciones_anonimas_callbacks.js
@@ -6,16 +6,19 @@
 	// Una funcion anonima es una funcion que no suele tener un nombre, mas bien se declara dentro de una variable.
 */
 
+/*
 var nameV = function(name){
 	document.write("<p>Name is: "+name+"</p>")
 };
 
 nameV("Joel");
+*/
 
 /*
 	Tambien tenemos los callbacks que no es mas que una llamada a una funcion dentro de otra.
 */
 
+/*
 
 function calcu(n1,n2,sum,resta,mult,div){
 	document.write("<p>Number 1: "+n1+"</p>");
@@ -34,4 +37,33 @@ calcu(7,3,function(dato){
 	document.write("<p>La multiplicación es: "+dato+"</p>");
 },function(dato){
 	document.write("<p>La división es: "+dato+"</p>");
+});
+
+*/
+
+
+	// Repaso de Funciones Anonimas y Callbacks....
+
+//Funcion Anonima
+
+var funcionA = function(name,age){
+	document.write("<p>Name is: "+name+"</p>");
+	document.write("<p>Age is: "+age+" years.</p>");
+}
+
+funcionA("Joel",20);
+
+//Callbacks
+
+function calcu(n1,n2,suma,resta){
+	document.write("<p>Number 1: "+n1+"</p>");
+	document.write("<p>Number 2: "+n2+"</p>");
+	suma(n1+n2);
+	resta(n1-n2);
+}
+
+calcu(7,2,function(dato){
+	document.write("<p>La suma es: "+dato+"</p>");
+},function(dato){
+	document.write("<p>La resta es: "+dato+"</p>");
 });
