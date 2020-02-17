511def0104ee8d5b77bed2293ecf01657a0303cc
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-16 22:49:48 -0600

    Funciones Flecha

---
+++ 2.Javascript/23-funcionesFlecha.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/*
+	Funciones Flecha!
+
+	Una funcion flecha nos sirve principalmente para ahorrar la estructura de una funcion.
+	Declaramos una variable donde se guardara el resultado de la funcion que queramos.
+*/
+
+var suma = (a,b) => (a+b);
+
+document.write("La suma es: "+suma(7,3));
