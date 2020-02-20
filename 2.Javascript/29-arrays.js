e23a12f0f76ae6cdbc88431f578f164b5703ad8d
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-19 22:14:13 -0600

    Arrays en JS

---
+++ 2.Javascript/29-arrays.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/*
+	19 - Feb - 2020 : 10:13 pm.
+	En esta clase trabajare con Arreglos / Arrays / Matrices Bidimensionales...
+	Sirven para guardar una cantidad de valores que necesitemos.
+*/
+
+// Opcion 1:
+
+var nombres = ["Joel","Keyla","Alazkin","Dudu"];
+console.log(nombres);
+
+document.write(`<p>Indice 2: ${nombres[2]}</p>`);
+
+// Opcion 2:
+
+var edades = new Array(20,12,1,2);
+console.log(edades);
+document.write(`<p>Edad en indice 1: ${edades[1]}</p>`);
