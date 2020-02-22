68fcb819f25ee7d4183527d60ed6b3bbfeecd685
Joel Dominguez Merino <50086105+joeldm011@users.noreply.github.com>
2020-02-22 15:52:57 -0600

    Update 31-foreach.js

--- 2.Javascript/31-foreach.js
+++ 2.Javascript/31-foreach.js
@@ -3,6 +3,7 @@
 /*
 	En esta clase trabajare con el arreglo ForEach en JS.
 	la funcion .foreach nos servira para recorrer ub arreglo y poder mostrar sus elementos del mismo.
+	Editado desde GitHub
 */
 
 var names = ["Joel","Julieta","Alazkin","Keyla","Marisela"];
