77d0e93451f949dbf6104a144507288df813bf22
Joel Dominguez Merino <joeldome17@gmail.com>
2020-02-21 20:41:07 -0600

    forEach JS

---
+++ 2.Javascript/31-foreach.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/*
+	En esta clase trabajare con el arreglo ForEach en JS.
+	la funcion .foreach nos servira para recorrer ub arreglo y poder mostrar sus elementos del mismo.
+*/
+
+var names = ["Joel","Julieta","Alazkin","Keyla","Marisela"];
+
+document.write("<ol>");
+names.forEach((i)=>{
+document.write("<li>"+i+"</li>");
+});
+document.write("</ol>");
